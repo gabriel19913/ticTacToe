fd0918d2337880c1c37c0419937d4ff7888b8642
Gabriel Fonseca <gabrielfonseca91@hotmail.com>
2018-06-11 23:47:20 -0300

    Code to skip from the loop working. Need more work in the draw situation

--- ticTacToe.py
+++ ticTacToe.py
@@ -64,8 +64,6 @@ def turn(turn):
         if positionList == []:
             winner()
             sys.exit()
-        print(positionList)
-        print(chosenList)
         p = True
         c = False
     if turn == 'cpu':
@@ -89,20 +87,7 @@ def turn(turn):
         if positionList == []:
             winner()
             sys.exit()
-        print(positionList)
-        print(chosen)
         print("My choice is " + position + ': ')
-
-        '''
-        while theboard.setdefault(position):
-            print("That's sad, someone already chose this position.")
-            print("A will choose again...")
-            if position in positionsList:
-                positionsList.remove(position)
-            print('.')
-            time.sleep(1)
-            position = random.choice(positionsList)
-            print("My new choice is " + position + ': ')'''
         c = True
         p = False
     return position, p, c
@@ -114,52 +99,70 @@ def move(position, player, cpu):
         theboard[position] = 'O'
     return theboard[position]
 
-
-'''
 def winner():
+    playerWin = False
+    cpuWin = False
+    draw = False
     values = list(theboard.values())
     for i in range(0, 7, 3):
         if (values[i] == values[i+1]) and (values[i+1] == values[i+2]):
             if values[i] == 'X':
-                print('*+*' * 18)
-                print("You have won the game! That's why you are my master!")
-                print('*+*' * 18)
+                playerWin = True
+                cpuWin = False
+                draw = False
             if values[i] == 'O':
-                print('*+*' * 18)
-                print("I have won the game! The creation beats its creator!")
-                print('*+*' * 18)
+                cpuWin = True
+                playerWin = False
+                draw = False
     for i in range(3):
         if (values[i] == values[i+3]) and (values[i+3] == values[i+6]):
             if values[i] == 'X':
-                print('*+*' * 18)
-                print("You have won the game! That's why you are my master!")
-                print('*+*' * 18)
+                playerWin = True
+                cpuWin = False
+                draw = False
             if values[i] == 'O':
-                print('*+*' * 18)
-                print("I have won the game! The creation beats its creator!")
-                print('*+*' * 18)
+                cpuWin = True
+                playerWin = False
+                draw = False
     for i in range(0, 3, 2):
         if i == 0:
             if (values[i] == values[i+4]) and (values[i+4] == values[i+8]):
                 if values[i] == 'X':
-                    print('*+*' * 18)
-                    print("You have won the game! That's why you are my master!")
-                    print('*+*' * 18)
+                    playerWin = True
+                    cpuWin = False
+                    draw = False
                 if values[i] == 'O':
-                    print('*+*' * 18)
-                    print("I have won the game! The creation beats its creator!")
-                    print('*+*' * 18)
+                    cpuWin = True
+                    playerWin = False
+                    draw = False
         if i == 2:
             if (values[i] == values[i+2]) and (values[i+2] == values[i+4]):
                 if values[i] == 'X':
-                    print('*+*' * 18)
-                    print("You have won the game! That's why you are my master!")
-                    print('*+*' * 18)
+                    playerWin = True
+                    cpuWin = False
+                    draw = False
                 if values[i] == 'O':
-                    print('*+*' * 18)
-                    print("I have won the game! The creation beats its creator!")
-                    print('*+*' * 18)
-'''
+                    cpuWin = True
+                    playerWin = False
+                    draw = False
+    if playerWin == False and cpuWin == False:
+        draw = True
+    return playerWin, cpuWin, draw
+
+def winnerMassage(pWin, cWin, d):
+    if pWin == True:
+        print('*+*' * 18)
+        print("You have won the game! That's why you are my master!")
+        print('*+*' * 18)
+    if cWin == True:
+        print('*+*' * 18)
+        print("I have won the game! The creation beats its creator!")
+        print('*+*' * 18)
+    if d == True:
+        print('*+*' * 18)
+        print("It's a draw!")
+        print('*+*' * 18)
+
 positionList = list(theboard.keys())
 menu()
 choice, face = start()
@@ -167,14 +170,12 @@ firstTurn, nextTurn = first(choice, face)
 while positionList:
     playerPosition, p, c = turn(firstTurn)
     move(playerPosition, p, c)
-    printBoard(theboard) 
+    printBoard(theboard)
+    pWin, cWin, d = winner()
+    if (pWin == True) or (cWin == True):
+        break
     playerPosition, p, c = turn(nextTurn)
     move(playerPosition, p, c)
     printBoard(theboard)
-    '''
-    for values in theboard.values():
-        if theboard.setdefault(values):
-            aux = 'end'
-    if aux == 'end':
-        break'''
-    #cpu continua jogando mesmo estando tudo completo
+    # I need to do a verification for draw situation
+winnerMassage(pWin, cWin, d)
